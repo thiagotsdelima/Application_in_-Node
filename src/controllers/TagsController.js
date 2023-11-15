6c3457cd028ad05c110e43e80202e784ae90cec2
thiago <thiagotsdelima@outlook.com>
2023-11-15 15:36:02 -0300

    Added initial project files

---
+++ src/controllers/TagsController.js
@@ -0,0 +1,11 @@
+const knex = require("../database/Knex");
+
+class TagsController{
+async index(request, response){
+const { user_id } = request.params;
+const tags = await knex("tags").where({ user_id });
+return response.json(tags);
+}
+}
+
+module.exports = TagsController;
